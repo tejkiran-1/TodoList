abd77e8d553051170e091b51f6f814a36ae41a2e
tejki <tejki@TEJKIRAN>
2024-07-07 18:57:10 +0530

    Add project files.

---
+++ TodoList/MainPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using TodoList.ViewModel;
+
+namespace TodoList
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage(MainViewModel vm)
+        {
+            InitializeComponent();
+
+            BindingContext = vm;
+        }
+
+    }
+
+}
---
+++ TodoList/ViewModel/MainViewModel.cs
@@ -0,0 +1,44 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System.Collections.ObjectModel;
+
+namespace TodoList.ViewModel
+{
+    public partial class MainViewModel : ObservableObject
+    {
+
+        public MainViewModel()
+        {
+            Items = new ObservableCollection<string>();
+        }
+
+
+        [ObservableProperty]
+        ObservableCollection<string> items;
+
+
+        [ObservableProperty]
+        string text;
+
+        [RelayCommand]
+        void Add()
+        {
+            if (string.IsNullOrWhiteSpace(Text))
+            {
+                return;
+            }
+            Items.Add(text);
+            Text = string.Empty;
+        }
+
+        [RelayCommand]
+        void Delete(string text)
+        {
+            if (Items.Contains(text))
+            {
+                Items.Remove(text);
+            }
+        }
+
+    }
+}
